a5ae79cc7846385fe718c7b6f0650b71fb145ab7
Bontle Hatebe <bontlehatebejames@gmail.com>
2024-11-22 14:28:05 +0200

    Removed Long HTML and Plugged it in using JS loop(NO API yet)

--- src/index.js
+++ src/index.js
@@ -69,9 +69,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 // Add an event listener to the search form to handle form submissions
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 // Default city to display weather information when the page loads
 searchCity("Paris");
+displayForecast();
